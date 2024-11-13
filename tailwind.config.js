79d4f132a1ce06c4e3c0374e07c1c0b4b2b328e6
Lara Scaranello <ti@grindelia.com.br>
2024-11-13 10:18:17 -0300

    CRM-1: installing mary-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typo from '@tailwindcss/typography';
+// import forms from '@tailwindcss/forms';
+// import typo from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        "./src/**/*.{html,js}"
-    ],
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		 './storage/framework/views/*.php',
+		 './resources/**/*.blade.php',
+		 './resources/**/*.js',
+		 './resources/**/*.vue',
+		 "./src/**/*.{html,js}",
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
     theme: {
         extend: {
             fontFamily: {
@@ -20,7 +21,8 @@ export default {
         },
     },
     plugins: [
-        forms,
-        typo
-    ],
+		// forms,
+		// typo,
+		require("daisyui")
+	],
 };

0188f031156dab66508886421e8b44dccefb987b
Lara Scaranello <ti@grindelia.com.br>
2024-11-14 09:40:33 -0300

    CRM-2: showing error message when attempting to login

--- tests/Feature/Livewire/Auth/LoginTest.php
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -10,7 +10,10 @@ it('renders successfully', function () {
 });
 
 it('should be able to login', function () {
-    $user = User::factory()->create(['email' => 'john@doe.com', 'password' => 'password']);
+    $user = User::factory()->create([
+        'email'    => 'john@doe.com',
+        'password' => 'password',
+    ]);
 
     Livewire::test(Login::class)
         ->set('email', 'john@doe.com')
@@ -21,4 +24,13 @@ it('should be able to login', function () {
 
     expect(auth()->check())->toBeTrue()
         ->and(auth()->user())->id->toBe($user->id);
+});
+
+it('should make sure to inform the user an error when email and password doesnt match', function () {
+    Livewire::test(Login::class)
+        ->set('email', 'john@doe.com')
+        ->set('password', 'password')
+        ->call('tryToLogin')
+        ->assertHasErrors(['invalidCredentials'])
+        ->assertSee(trans('auth.failed'));
 });

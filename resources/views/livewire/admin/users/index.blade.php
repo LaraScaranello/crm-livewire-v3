8768e2470e7d343165bc3866ebffdf8f78876466
Lara Scaranello <ti@grindelia.com.br>
2025-06-21 10:06:49 -0300

    CRM-12: checking the table format

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -1,5 +1,15 @@
 <div>
-    @foreach($this->users as $users)
-        <li>{{ $users->name }}</li>
-    @endforeach
+    <x-header title="Users" separator progress-indicator/>
+
+    <x-table :headers="$this->headers" :rows="$this->users">
+        @scope('cell_permissions', $user)
+        @foreach($user->permissions as $permission)
+            <x-badge :value="$permission->key" class="badge-primary"/>
+        @endforeach
+        @endscope
+
+        @scope('actions', $users)
+        <x-button icon="o-trash" wire:click="delete({{ $users->id }})" spinner class="btn-sm"/>
+        @endscope
+    </x-table>
 </div>
--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -2,7 +2,7 @@
 
 use App\Livewire\Admin;
 use App\Models\User;
-use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Livewire;
 
 use function Pest\Laravel\{actingAs, get};
 
@@ -20,13 +20,13 @@ test('making sure that the route s oteted by the permission BE_AN_ADMIN', functi
 });
 
 test("let's create a livewire component to list all users in the page", function () {
+    actingAs(User::factory()->admin()->create());
     $users = User::factory()->count(10)->create();
 
     $lw = Livewire::test(Admin\Users\Index::class);
     $lw->assertSet('users', function ($users) {
         expect($users)
-            ->toBeInstanceOf(LengthAwarePaginator::class)
-            ->toHaveCount(10);
+            ->toHaveCount(11);
 
         return true;
     });
@@ -34,4 +34,16 @@ test("let's create a livewire component to list all users in the page", function
     foreach ($users as $user) {
         $lw->assertSee($user->name);
     }
+});
+
+test('check the table format', function () {
+    actingAs(User::factory()->admin()->create());
+
+    Livewire::test(Admin\Users\Index::class)
+        ->assertSet('headers', [
+            ['key' => 'id', 'label' => '#'],
+            ['key' => 'name', 'label' => 'Name'],
+            ['key' => 'email', 'label' => 'Email'],
+            ['key' => 'permissions', 'label' => 'Permissions'],
+        ]);
 });

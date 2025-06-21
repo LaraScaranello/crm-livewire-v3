202bde8274eef6e849d531560660b9ec0633132f
Lara Scaranello <ti@grindelia.com.br>
2025-06-21 11:40:03 -0300

    CRM-12: frontend: using choice component to filter permissions

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -7,12 +7,19 @@
                 icon="o-magnifying-glass"
                 wire:model.live="search"
                 placeholder="Search by name and email"
-                class="input-sm"
             />
         </div>
-        <x-select class="select-sm">
-            <option value="1">1</option>
-        </x-select>
+        <div class="w-1/3">
+            <x-choices
+                placeholder="Filter by permissions"
+                wire:model.live="search_permissions"
+                :options="$permissionsToSearch"
+                option-label="key"
+                search-function="filterPermissions"
+                searchable
+                no-result-text="Nothing here"
+            />
+        </div>
     </div>
 
     <x-table :headers="$this->headers" :rows="$this->users">

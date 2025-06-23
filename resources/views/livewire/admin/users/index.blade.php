c4b94a0bb4d6218321e7db25a5f0f7d992b04820
Lara Scaranello <ti@grindelia.com.br>
2025-06-23 09:50:12 -0300

    CRM-12: frontend: add checkbox to filter trashed users

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -20,6 +20,11 @@
                 no-result-text="Nothing here"
             />
         </div>
+        <x-checkbox
+            label="Show deleted users"
+            wire:model.live="search_trash"
+            class="checkbox-primary"
+            right tight/>
     </div>
 
     <x-table :headers="$this->headers" :rows="$this->users">
@@ -30,7 +35,12 @@
         @endscope
 
         @scope('actions', $users)
-        <x-button icon="o-trash" wire:click="delete({{ $users->id }})" spinner class="btn-sm"/>
+        @unless($users->trashed())
+            <x-button icon="o-trash" wire:click="delete({{ $users->id }})" spinner class="btn-sm"/>
+        @else
+            <x-button icon="o-arrow-path-rounded-square" wire:click="restore({{ $users->id }})" spinner
+                      class="btn-sm btn-success btn-ghost"/>
+        @endunless
         @endscope
     </x-table>
 </div>

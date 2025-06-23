9f3d4aa39d70e85a3191b6b3ac60871f17aa60c4
Lara Scaranello <ti@grindelia.com.br>
2025-06-23 17:08:50 -0300

    CRM-12: frontend: pagination

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -24,7 +24,13 @@
             label="Show deleted users"
             wire:model.live="search_trash"
             class="checkbox-primary"
-            right tight/>
+            right tight
+        />
+        <x-select
+            wire:model.live="perPage"
+            :options="[['id'=>5,'name'=>'5'],['id'=>15,'name'=>'15'],['id'=>25,'name'=>'25'],['id'=>50,'name'=>'50']]"
+            label="Records per page"
+        />
     </div>
 
     <x-table :headers="$this->headers" :rows="$this->users">
@@ -55,4 +61,6 @@
         @endunless
         @endscope
     </x-table>
+
+    {{ $this->users->links() }}
 </div>

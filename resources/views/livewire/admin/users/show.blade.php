7c38b656b5ae7deb3f678fa2c70902402afa30ae
Lara Scaranello <ti@grindelia.com.br>
2025-06-26 11:49:30 -0300

    crm-13: displaying the information inside a drawer instead of a modal

--- resources/views/livewire/admin/users/show.blade.php
+++ resources/views/livewire/admin/users/show.blade.php
@@ -1,23 +1,21 @@
-<div>
-    <x-modal
-        wire:model="modal"
-        :title="$user?->name ?? 'User'"
-        separator
-        class="backdrop-blur">
+<x-drawer
+    wire:model="modal"
+    :title="$user?->name ?? 'User'"
+    separator right
+    class="w-1/3 p-6">
 
-        @if($user)
-            <div class="space-y-3">
-                <x-input readonly label="Name" :value="$user->name"/>
-                <x-input readonly label="Email" :value="$user->email"/>
-                <x-input readonly label="Created At" :value="$user->created_at->format('d/m/Y H:i')"/>
-                <x-input readonly label="Updated At" :value="$user->updated_at?->format('d/m/Y H:i')"/>
-                <x-input readonly label="Deleted At" :value="$user->deleted_at?->format('d/m/Y H:i')"/>
-                <x-input readonly label="Deleted By" :value="$user->deletedBy?->name"/>
-            </div>
-        @endif
+    @if($user)
+        <div class="space-y-3">
+            <x-input readonly label="Name" :value="$user->name"/>
+            <x-input readonly label="Email" :value="$user->email"/>
+            <x-input readonly label="Created At" :value="$user->created_at->format('d/m/Y H:i')"/>
+            <x-input readonly label="Updated At" :value="$user->updated_at?->format('d/m/Y H:i')"/>
+            <x-input readonly label="Deleted At" :value="$user->deleted_at?->format('d/m/Y H:i')"/>
+            <x-input readonly label="Deleted By" :value="$user->deletedBy?->name"/>
+        </div>
+    @endif
 
-        <x-slot:actions>
-            <x-button label="Cancel" @click="$wire.modal = false"/>
-        </x-slot:actions>
-    </x-modal>
-</div>
+    <x-slot:actions>
+        <x-button label="Cancel" @click="$wire.modal = false"/>
+    </x-slot:actions>
+</x-drawer>

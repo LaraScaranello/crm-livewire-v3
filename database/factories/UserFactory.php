94920fa696aeeabf6ec7434df1746a0ba5418c0e
Lara Scaranello <ti@grindelia.com.br>
2025-08-09 11:03:57 -0300

    crm-11: validation page: checking if the code is valid

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -51,6 +51,13 @@ class UserFactory extends Factory
         );
     }
 
+    public function withValidationCode(Can ...$keys): static
+    {
+        return $this->state(fn () => [
+            'validation_code' => random_int(100000, 999999),
+        ]);
+    }
+
     public function admin(): static
     {
         return $this->afterCreating(fn (User $user) => $user->givePermissionTo(Can::BE_AN_ADMIN));

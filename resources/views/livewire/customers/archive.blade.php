1aef433bc61c9ccf093f58e836635b921d501b32
Lara Scaranello <ti@grindelia.com.br>
2025-09-05 09:25:11 -0300

    crm-20: telling the modal to close and the list to reload

--- resources/views/livewire/customers/archive.blade.php
+++ resources/views/livewire/customers/archive.blade.php
@@ -1,3 +1,13 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <x-modal
+        wire:model="modal"
+        title="Archive Cofirmation"
+        subtitle="You are archiving the customer {{ $customer?->name }}"
+        class="backdrop-blur">
+
+        <x-slot:actions>
+            <x-button label="Hum... no" @click="$wire.modal = false"/>
+            <x-button label="Yes, I am" class="btn-primary" wire:click="archive"/>
+        </x-slot:actions>
+    </x-modal>
 </div>
